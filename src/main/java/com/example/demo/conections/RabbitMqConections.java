12d703c22f40ba1218e15996cad67cd9c8389fa6
Eduardo C. Smith <eduardocsmith021@gmail.com>
2024-08-10 20:39:55 -0400

    Conclusão das filas Estoque e Preço, conexão com RabbitMq

--- src/main/java/com/example/demo/conections/RabbitMqConections.java
+++ src/main/java/com/example/demo/conections/RabbitMqConections.java
@@ -1,5 +1,6 @@
 package com.example.demo.conections;
 
+import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
@@ -8,8 +9,16 @@ import org.springframework.stereotype.Component;
 
 import com.example.demo.constante.RabbitMqConstantes;
 
+import jakarta.annotation.PostConstruct;
+
 @Component
 public class RabbitMqConections {
+
+    private AmqpAdmin amqpAdmin;
+
+    public RabbitMqConections(AmqpAdmin amqpAdmin){
+        this.amqpAdmin = amqpAdmin;
+    }
     
     private static final String NOME_EXCHANGE = "amq.direct"; //Nome da amq.direct que está na Exchange do rabbitmq
 
@@ -20,10 +29,29 @@ public class RabbitMqConections {
         return  new  DirectExchange(NOME_EXCHANGE);
     }
     private Binding relacionamento(Queue fila, DirectExchange troca){
-        return new Binding(fila.getName(), Binding.DestinationType.EXCHANGE, troca.getName(), fila.getName(), null);
+        return new Binding(fila.getName(), Binding.DestinationType.QUEUE, troca.getName(), fila.getName(), null);
     }
+
+    @PostConstruct  //Atonação para fazer que o metodo adiciona inicie assim que levantar a aplicação
     private void adiciona(){
-        this.fila(RabbitMqConstantes.FILA_ESTOQUE);
+        Queue filaEstoque = this.fila(RabbitMqConstantes.FILA_ESTOQUE);
+        Queue filaPreco = this.fila(RabbitMqConstantes.FILA_PRECO);
+
+        DirectExchange troca = trocaDireta();
+
+        Binding ligacaoEstoque = this.relacionamento(filaEstoque, troca);
+        Binding ligacaoPreco = this.relacionamento(filaPreco, troca);
+
+        //Criando fila no RabbitMq
+        this.amqpAdmin.declareQueue(filaEstoque);
+        this.amqpAdmin.declareQueue(filaPreco);
+
+        //Criando Exchange
+        this.amqpAdmin.declareExchange(troca);
+
+        //Criando relacionamento
+        this.amqpAdmin.declareBinding(ligacaoEstoque);
+        this.amqpAdmin.declareBinding(ligacaoPreco);
     }
 }
  
