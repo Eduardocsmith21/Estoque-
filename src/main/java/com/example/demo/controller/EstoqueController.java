1eb22f9d5bee4745d269cb303e9365adf492b847
Eduardo C. Smith <eduardocsmith021@gmail.com>
2024-08-12 14:04:15 -0400

    Finalização da API

--- src/main/java/com/example/demo/controller/EstoqueController.java
+++ src/main/java/com/example/demo/controller/EstoqueController.java
@@ -1,21 +1,27 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.constante.RabbitMqConstantes;
 import com.example.demo.dto.EstoqueDto;
+import com.example.demo.service.RabbitMqServer;
 
 @RestController //Mostra a c classe vai ser exporta por uma API 
 @RequestMapping (value = "/estoque")     //Mapeia a classe por meio de em endpoint
 
 public class EstoqueController {
 
+    @Autowired
+    private RabbitMqServer rabbitMqServer;
+
     @RequestMapping("/alteraEstoque")
-    private ResponseEntity<String> alteraEstoque (@RequestBody EstoqueDto estoqueDto){        //ResponseEntity - Representa a  resposta HTTP
+    private ResponseEntity<String> alteraEstoque (@RequestBody EstoqueDto estoqueDto){        //ResponseEntity - Representa a  resposta HTT
+        this.rabbitMqServer.enviaMensagem(RabbitMqConstantes.FILA_ESTOQUE, estoqueDto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/example/demo/controller/PrecoController.java
+++ src/main/java/com/example/demo/controller/PrecoController.java
@@ -1,21 +1,28 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.constante.RabbitMqConstantes;
 import com.example.demo.dto.EstoqueDto;
+import com.example.demo.service.RabbitMqServer;
 
 @RestController
 @RequestMapping(value = "/preco")
 
 public class PrecoController {
 
+    @Autowired
+    private RabbitMqServer rabbitMqServer;
+
     @RequestMapping("/alteraPreco")
     private ResponseEntity<String> alterarPreco(@RequestBody EstoqueDto estoqueDto){
+        System.out.println(estoqueDto);
+        this.rabbitMqServer.enviaMensagem(RabbitMqConstantes.FILA_PRECO, estoqueDto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
     
